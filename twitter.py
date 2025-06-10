395508b9542377fc19ec8ed80439115f919d2d8a
ibrahim <57787419+ibrahimakcayy@users.noreply.github.com>
2025-06-10 21:05:30 +0300

    Update twitter.py
    add sessionid fixed bugs

--- twitter.py
+++ twitter.py
@@ -4,12 +4,13 @@ from selenium import webdriver as web
 import time
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
 #useless
 def cont():
     
-    return "ibrahim-twitter.py"
+    return "ibrahim-twitter(X).py"
 
 
 #get driver from main page
@@ -39,41 +40,54 @@ def xpaths(name,paths):
 
 
 #login twitter
-def login(usern,passw):
+def login(s="usermane",usern="username",passw="password",key="sessionid"):
     
-    #write username
-    driver.find_element(By.XPATH,xpaths("x-xpath",0)).send_keys(usern)
-    time.sleep(0.3)
+    if s=="username":
+        
+        #write username
+        driver.find_element(By.XPATH,xpaths("x-xpath",0)).send_keys(usern)
+        time.sleep(0.3)
+    
+        #click next button
+        driver.find_element(By.XPATH,xpaths("x-xpath",1)).click()
+        time.sleep(5)
+    
+        #write password
+        driver.find_element(By.XPATH,xpaths("x-xpath",2)).send_keys(passw)
+        time.sleep(0.3)
+    
+        #click login button
+        driver.find_element(By.XPATH,xpaths("x-xpath",3)).click()
+        time.sleep(1)
+        print("Logged in with using username & password")
+        
+    #using sessionid to bypass login progress
+    else:
 
-    #click next button
-    driver.find_element(By.XPATH,xpaths("x-xpath",1)).click()
-    time.sleep(5)
-
-    #write password
-    driver.find_element(By.XPATH,xpaths("x-xpath",2)).send_keys(passw)
-    time.sleep(0.3)
-
-    #click login button
-    driver.find_element(By.XPATH,xpaths("x-xpath",3)).click()
-    time.sleep(1)
-
-    print("Logged in")
+        #set cookie
+        driver.add_cookie({"name": "auth_token", "domain":".x.com", "value": key})
+        driver.refresh()
+        time.sleep(5)
+        print("Logged in with using sessionid")
 
 
 #share tweet (with pic or without)
-def share_tweet(tweet_text,photo_url="None"):
+def share_tweet(tweet_text,photo_url="none"):
 
     #again open twitter
-    driver.get("https://twitter.com/home")
     time.sleep(5)
     print("Start Sharing")
+
 
     #write tweet
     driver.find_element(By.XPATH,xpaths("x-xpath",4)).send_keys(tweet_text)
     time.sleep(1)
 
     #if you want to tweet with photo photo_url chage false to url
-    if photo_url!="None":
+    if photo_url!="none":
+        
+        #photo Keys.CONTROL + 'v'
+        #driver.find_element(By.XPATH,xpaths("x-xpath",4)).send_keys(Keys.CONTROL + 'v')
         
         #enter photo
         driver.find_element(By.XPATH,xpaths("x-xpath",5)).send_keys(photo_url)
